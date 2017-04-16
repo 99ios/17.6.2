31d74465238395233b91fb41abd4ff15b9d4b959
test <422538722@qq.com>
2017-04-16 16:57:34 +0800

    17.6.2
    发送短信功能

---
+++ 17.6.2 发送短信功能/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  17.6.2 发送短信功能
+//
+//  Created by 李维佳 on 2017/4/14.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
